6bc262549fa3e54945856bec5842cad2065db64a
Sofia <sofiya.lidskya@gmail.com>
2024-08-21 19:51:10 +0300

    NavBar

--- client/src/components/ui/NavBar.jsx
+++ client/src/components/ui/NavBar.jsx
@@ -4,40 +4,40 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
+import Stack from 'react-bootstrap/Stack';
 
 export default function NavBar({ user, logoutHandler }) {
-
   return (
-    <Navbar bg="light" data-bs-theme="light">
-      <Container>
-        <Nav className="me-auto">
-          <NavLink to="/" className="nav-link">
-            Главная
-          </NavLink>
-
-        </Nav>
+    <Navbar style={{ backgroundColor: "#0d6efd", color: "white"}} data-bs-theme="blue">
+      <Container >
+        <Nav.Link href="/">
+          <img
+            src="/1500485760140135437 (1).png"
+            alt="Logo"
+            style={{ height: '40px', width: 'auto' }}
+          />
+        </Nav.Link>
         <Nav>
-          {!user.data && (
+          {user.data ? (
+            <div style={{ display: "flex", 'align-items':'center'}}>
+              <span style={{ color: "white"}} className="nav-link">{user.data.name}</span>
+              <span className="nav-link">
+                <Button style={{ color: "white"}} onClick={logoutHandler} size="sm">
+                  Выйти
+                </Button>
+              </span>
+            </div>
+          ) : (
             <>
-              <NavLink to="/account/login" className="nav-link">
-              Войти
+              <NavLink style={{ marginRight: '40px', color: "white" }} to="/account/login" className="nav-link">
+                Войти
               </NavLink>
-              <NavLink to="/account/new" className="nav-link">
-              Регистрация
-              </NavLink>
-              <span className="nav-link">|</span>
-            </>
-          )}
 
-          <span className="nav-link">
-            {user.data ? user.data.name : 'Гость'}
-          </span>
-          {user.data && (
-            <span className="nav-link">
-              <Button onClick={logoutHandler} variant="outline-danger" size="sm">
-                Выйти
-              </Button>
-            </span>
+              <NavLink style={{ color: "white"}} to="/account/new" className="nav-link">
+                Регистрация
+              </NavLink>
+              <span style={{ display: 'flex', 'align-items': 'center', marginLeft: '870px', marginRight: '30px' }}>Гость</span>
+            </>
           )}
         </Nav>
       </Container>
