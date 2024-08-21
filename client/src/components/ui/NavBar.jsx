b0d47e421e64b62bb038631eb6ce185c3c0841ca
Sofia <sofiya.lidskya@gmail.com>
2024-08-21 22:58:23 +0300

    Cделала выпадающее меню в личном кабинете

--- client/src/components/ui/NavBar.jsx
+++ client/src/components/ui/NavBar.jsx
@@ -1,45 +1,56 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Button, Container, Nav, Navbar, Dropdown } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
-import Stack from 'react-bootstrap/Stack';
 
 export default function NavBar({ user, logoutHandler }) {
   return (
-    <Navbar style={{ backgroundColor: "#0d6efd", color: "white"}} data-bs-theme="blue">
-      <Container >
-        <Nav.Link href="/">
+    <Navbar expand="lg" style={{ backgroundColor: "#0d6efd", color: "white" }} data-bs-theme="blue">
+      <Container>
+        <Navbar.Brand href="/">
           <img
             src="/1500485760140135437 (1).png"
             alt="Logo"
             style={{ height: '40px', width: 'auto' }}
           />
-        </Nav.Link>
-        <Nav>
-          {user.data ? (
-            <div style={{ display: "flex", 'align-items':'center'}}>
-              <span style={{ color: "white"}} className="nav-link">{user.data.name}</span>
-              <span className="nav-link">
-                <Button style={{ color: "white"}} onClick={logoutHandler} size="sm">
-                  Выйти
-                </Button>
-              </span>
-            </div>
-          ) : (
-            <>
-              <NavLink style={{ marginRight: '40px', color: "white" }} to="/account/login" className="nav-link">
-                Войти
-              </NavLink>
+        </Navbar.Brand>
 
-              <NavLink style={{ color: "white"}} to="/account/new" className="nav-link">
-                Регистрация
-              </NavLink>
-              <span style={{ display: 'flex', 'align-items': 'center', marginLeft: '870px', marginRight: '30px' }}>Гость</span>
-            </>
-          )}
-        </Nav>
+        <span style={{ color: "white" }}>
+        {user.data?.name ? `Привет, ${user.data.name}!` : ""}
+         
+        </span>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            {user.data ? (
+              <Dropdown align="end">
+                <Dropdown.Toggle variant="link" className="nav-link" style={{ color: "white" }}>
+                  Личный кабинет
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#">Мои достижения</Dropdown.Item>
+                  <Dropdown.Item as="span" onClick={logoutHandler}>
+                    Выйти
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            ) : (
+              <>
+                <NavLink style={{ marginRight: '40px', color: "white" }} to="/account/login" className="nav-link">
+                  Войти
+                </NavLink>
+                <NavLink style={{ color: "white" }} to="/account/new" className="nav-link">
+                  Регистрация
+                </NavLink>
+                <span style={{ display: 'flex', alignItems: 'center', marginLeft: '870px', marginRight: '30px' }}>
+                  Гость
+                </span>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
